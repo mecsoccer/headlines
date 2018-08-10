53756b864806ed4096bfec9a5ad6eb3083aff66e
Jaachimma Onyenze <onyenze.jaachimma@gmail.com>
2018-08-10 07:27:06 +0100

    first offline working version.

--- public/javascripts/newsController.js
+++ public/javascripts/newsController.js
@@ -1,48 +1,111 @@
-var dbpromise = idb.open('news', 1, function(upgradedb){
-	var store = upgradedb.createObjectStore('headlines', {keyPath: 'author'});
-	store.createIndex('time', 'publishedAt');
-});
+var openIDB = function(){
+	//if no serviceWorker no need for a db
+	if (!navigator.serviceWorker) {
+		return Promise.resolve();
+	}
+	
+	return idb.open('Headlines', 1, function(upgradeDb){
+		var store = upgradeDb.createObjectStore('headlines');
+		store.createIndex('time', 'publishedAt');
+	});
+}
 
-mylist = document.querySelector('.headline');
+var Newscard = function(queryString){
+	this.queryString = queryString || "country=ng";
+	this.url = 'https://newsapi.org/v2/top-headlines?' + this.queryString + '&apiKey=f5b8df00fbc34645b92e985a0e575e29';
+	this.timeLastUpdated = new Date();
+	this.openIDB = openIDB();
+	this.sources = [];
+	this.countries = [];
+	this.showingLatest = false;
+}
 
-var dbdata = [];
-var fetchdata = [];
-
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=ng&' +
-          'apiKey=f5b8df00fbc34645b92e985a0e575e29';
-
-var req = new Request(url);
-
-fetch(req).then(function(response){
-	if (response) return response.json();
-	return
-}).then(function(news){
-	news.articles.forEach(function(article){
-		fetchdata.push(article)
-	})
-})
-
-dbpromise.then(function(db){
-	return db.transaction('headlines', 'readwrite')
-		 .objectStore('headlines').index('time').getAll()
-}).then(function(articles){
-	articles.forEach(function(article){
-		dbdata.push(article)
-	})
-}).then(function(){
-	return addPosts(dbdata.reverse())
-}).then(function(){
-	return addPosts(fetchdata);
-});
-
-function addPosts(articleArray){
-	articleArray.forEach(function(article){
+//attach UI element with news to html layout
+Newscard.prototype.renderData = function(data){
+	var headlines = document.querySelector('.headline');
+	data.forEach(function(article){
 		var listItem = document.createElement('li');
 		listItem.innerHTML = '<img src="' + article.urlToImage 
 							 + '" alt="news pic"><div><strong><a class="title" href="' 
 							 + article.url + '" target="_blank">' + article.title
 			                 + '</a></strong><p>' + article.description + '...' + '</p></div>'
-		mylist.appendChild(listItem);
+		headlines.appendChild(listItem);
+	});
+}
+
+//get data from network
+Newscard.prototype.networkResponse = function(){
+	var newscard = this;
+	var req = new Request(newscard.url);
+	fetch(req).then(function(response){
+		return response.json();
+	}).then(function(news){
+		newscard.renderData(news.articles);
+		newscard.showingLatest = true;
+		return news.articles;
+	}).then(function(articles){
+		// store data in db immediately after network response 
+		newscard.openIDB().then(function(db){
+			var dbTransaction = db.transaction('headlines', 'readwrite');
+			dbTransaction.objectStore('headlines').put(articles, newscard.queryString);
+		});
+	});
+}
+
+//get news from network and render to the browser
+Newscard.prototype.getNews = function () {
+	//get current time and time for last network response
+	var currentTime = new Date();
+	currentTime = currentTime.getTime();
+	this.timeLastUpdated = this.timeLastUpdated.getTime();
+	
+	var newscard = this;
+	
+	//if browser supports caching, check if service worker has cached this request 
+	//before then respond with cached data	
+	if ('caches' in window) {
+		caches.match(newscard.url).then(function(response){
+			if (response) return response.json();
+		}).then(function(news){
+			newscard.renderData(news.articles);
+		});
+	}
+	
+	return newscard.renderData(dummyData) || newscard.networkResponse();
+}
+
+//get from cache if available 
+Newscard.prototype.cacheResponse = function(){
+	//add cache logic here
+	return 'nothing in cache';
+}
+
+//get news from indexedDb
+Newscard.prototype.dbResponse = function(){
+	openIDB().then
+}
+
+//data to render if no network. this is for testing only
+var dummyData = [
+  { urlToImage: '/images/jaachi.jpg',
+	url: '#',
+	title: 'Jaachimma abiakwala nu ozo oo. ',
+	description: 'Onyeoma afutala kwuo na onye obula nuru ube nwanne ya agbakwala oso.'
+  }, { urlToImage: '/images/jaachi.jpg',
+	url: '#',
+	title: 'Jaachimma abiakwala nu ozo oo. ',
+	description: 'Onyeoma Jaachimma afutala kwuo na onye obula nuru ube nwanne ya agbakwala oso.'+
+	             ' O sitere na otu aka ahu kwuo na izu ka mma na nneji'
+  }
+]
+
+var News = new Newscard();
+News.getNews();
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js').then(function(reg){
+		console.log('service worker regitered. scope: ', reg.scope);
+	}).catch(function(err){
+		console.log('error occured');
 	})
 }
--- public/sw.js
+++ public/sw.js
@@ -1,38 +1,51 @@
-var StaticCacheName = 'headlines-static-v1';
+var cacheName = 'headlinesPWA-v1';
+var dataCacheName = 'headlinesData-v1';
+var allCaches = [cacheName, dataCacheName];
 
 self.addEventListener('install', function(event){
+	console.log('[service worker] install');
 	event.waitUntil(
-	  caches.open(StaticCacheName).then(function(cache){
-		  return cache.addAll([
-		    '/javascripts/idb.js',
-			'/stylesheets/responsive.css',
-			'/stylesheets/style.css',
-			'http://weloveiconfonts.com/api/fonts/zocial/zocial-regular-webfont.woff'
-		  ]);
-	  })
-	)
-})
-
-self.addEventListener('activate', function(event){
-	event.waitUntil(
-	  caches.keys().then(function(cacheNames){
-		Promise.all(
-		  cacheNames.filter(function(cacheName){
-			  return cacheName.startsWith('headlines-') &&
-			         cacheName != StaticCacheName
-		  }).map(function(cacheName){
-			  return cache.delete(cacheName);
-		  })
-		)  
-	  })
-	)
-})
-
-self.addEventListener('fetch', function(event){
-	event.respondWith(
-	  caches.match(event.request).then(function(response){
-		  if (response) return response;
-		  return fetch(event.request);
+	  caches.open(cacheName).then(function(cache){
+		  console.log('[service worker] caching app shell');
+		  return cache.addAll(['/', '/news', '/news/countries', '/news/sources', '/images/jaachi.jpg',
+		    '/images/logo.png', '/javascripts/idb.js', '/javascripts/newsController.js', '/stylesheets/style.css', 
+			'/stylesheets/responsive.css']);
 	  })
 	);
-})
+});
+
+self.addEventListener('activate', function(event){
+	console.log('[service worker] activate');
+	event.waitUntil(
+	  caches.keys().then(function(keyList){
+		  return Promise.all(keyList.map(function(key){
+			  if (key !== cacheName && key !== dataCacheName) {
+				  console.log('[service worker] deleting old cache: ', key);
+				  return caches.delete(key);
+			  }
+		  }));
+	  })
+	);
+	return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+	console.log('[service worker] fetch', event.request.url);
+	var apiKey = 'f5b8df00fbc34645b92e985a0e575e29';
+	if (event.request.url.includes(apiKey)) {
+		event.respondWith(
+		  caches.open(dataCacheName).then(function(cache){
+			  return fetch(event.request).then(function(response){
+				  cache.put(event.request.url, response.clone());
+				  return response;
+			  });
+		  })
+		);
+	} else {
+		event.respondWith(
+		  caches.match(event.request).then(function(response){
+			  return response || fetch(event.request);
+		  })
+		);
+	}
+});
